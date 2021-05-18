129f0bb3337af6aa808893b83f22bda22bfad5bd
Jake Adler <adlerrjake@gmail.com>
2021-05-18 11:27:10 +0400

    Fixing Images
    Adjusting images to fit my portfolio.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hey! This is my crypto portfolio.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jakeradler.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click Here
         </a>
       </header>
     </div>
